3803617a64caafb06bd24fe353087bb535410e71
chetanachaudhary <chetanachaudhary445@gmail.com>
2024-04-12 16:34:32 +0545

    someChanges

--- einsurance.client/src/App.jsx
+++ einsurance.client/src/App.jsx
@@ -32,18 +32,10 @@ function App() {
         </table>;
 
     return (
-        <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
-    );
+       <div></div>
+    )
     
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
-}
+   
+  
 
 export default App;
