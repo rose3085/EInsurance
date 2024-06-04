e757b53c9f394aa2c25f866a57c908ce08aa845c
chetanachaudhary <chetanachaudhary445@gmail.com>
2024-06-04 15:44:50 +0545

    comparsion part completed

--- einsurance.client/src/components/FilterPolicy/Compare.jsx
+++ einsurance.client/src/components/FilterPolicy/Compare.jsx
@@ -1,139 +1,210 @@
 import './Compare.css';
 import filter from '../Icons/filter2.png';
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 /*import Select from 'react-select';*/
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, NavLink } from 'react-router-dom';
 import { useResponse } from '../../context/ResponseContext';
-
-const Compare = () =>
-{
-    const [selectedPolicy, setSelectedPolicy] = useState('');
-
-    const handleSelectChange = (event) => {
-        // const selectedOptions = Array.from(event.target.selectedOptions, (option) => option.value);
-        // const limitedSelection = selectedOptions.slice(0, 2);
-      setSelectedPolicy(event.target.value);
-    };
+import { useParams } from 'react-router-dom';
+import cancel from '../Icons/CancelButton.png';
+import phoneCall from '../Icons/phoneCall.png';
+import internet from '../Icons/internet.png';
+import insuranceIcon from '../Icons/insuranceIcon.png';
 
 
-    const [selectedOptions, setSelectedOptions] = useState([]);
-
-    const handleCheckboxChange = (policyName) => {
-      if (selectedOptions.includes(policyName)) {
-        setSelectedOptions(selectedOptions.filter((option) => option !== policyName));
-      } else if (selectedOptions.length < 2) {
-        setSelectedOptions([...selectedOptions, policyName]);
-      }
-    };
-  
-    const handleSubmit = () => {
-       
-            if (selectedOptions.length === 2) {
-              const [policyName1, policyName2] = selectedOptions;
-            //   const url = `/CompareResult/${encodeURIComponent(policyName1)}/${encodeURIComponent(policyName2)}`;
-            //   navigate(url);
-
-            navigate(`/CompareResult/${encodeURIComponent(policyName1)}`, { state: { policyName1, policyName2 } });
-      // navigate('/Page2', { state: { policyName1, policyName2 } });
-
-            // onSubmit(policyName1, policyName2);
-            // navigate(`/CompareResult`);
-
-            }
-        
-      };
-    
-
+const Compare = () => {
+    const { policyName } = useParams();
     const { responseData } = useResponse();
-    const navigate = useNavigate();
+   /* const [selectedOptions, setSelectedOptions] = useState([]);*/
+    const [selectedPolicyDetails, setSelectedPolicyDetails] = useState(null);
+
+
+
+    // State to keep track of the active link
+    const [activePolicy, setActivePolicy] = useState(null);
+
+   
+
+   
+
+    const handleClick = (policyName) => {
+        const selectedPolicy = responseData.find((policy) => policy.policyName === policyName);
+        setSelectedPolicyDetails(selectedPolicy);
+        setActivePolicy(policyName);
+    };
+
+    // useEffect to reset selected details when responseData changes
+    useEffect(() => {
+        setSelectedPolicyDetails(null);
+    }, [responseData]);
+
+ 
+
+
+
 return (
     <section className="mainComponentCompare">
-    <div className="summaryComponent">
-        <div className="filters">
-            <div className="filterImg"><img src={filter}  alt="" height='23px' id='checked'/> </div>
-            <div><p className="filterText">Compare Policy</p></div>
-        </div>
-    <div className="line"> </div>
-   
-    {/* {responseData ? responseData.map((responseValue, index) => { 
-        return(
-            <div className="policyCompareName">
-               
-                <div className="minCover">{responseValue.policyName} </div>
+        <div className="summarysComponent">
+            <div className="filters">
+                <div className="filterImg">
+                    <img src={filter} alt="" height='23px' id='checked' /> </div>
+                <div><p className="filterText">Compare Policy</p></div>
             </div>
-        ); }) : []} */}
+            <div className="line"> </div>
 
 
-    <div className="selectPolicy">
-      {/* <select className='h-8 w-60 m-2 rounded-sm text-lg'value={selectedPolicy} onChange={handleSelectChange}>
-        <option value="">Select a policy</option>
-        {responseData &&
-          responseData.map((responseValue, index) => (
-            <option key={index} value={responseValue.policyName}>
-              {responseValue.policyName}
-            </option>
-          ))}
-      </select>
 
-      {responseData &&
-        responseData.map((responseValue, index) => (
-          <div key={index} className="policyCompareName">
-            {selectedPolicy === responseValue.policyName && (
-              <div className="minCover">{responseValue.policyName}</div>
-            )}
-          </div>
-        ))} */}
 
-         {responseData.map((responseValue, index) => (
-        <div className="policyCompareName" key={index}>
-          <input
-            type="checkbox"
-            id={`policy-${index}`}
-            value={responseValue.policyName}
-            checked={selectedOptions.includes(responseValue.policyName)}
-            onChange={() => handleCheckboxChange(responseValue.policyName)}
-          />
-          <label htmlFor={`policy-${index}`}>{responseValue.policyName}</label>
+            <div className="selectPolicy">
+
+
+                {responseData.map((responseValue, index) => (
+                    <div className="policyCompareName" key={index}>
+                        <NavLink
+                            key={responseValue.policyName}
+                            onClick={() => handleClick(responseValue.policyName)}
+                            className={() =>
+                                `${responseValue.policyName === activePolicy ? "text-[#0065ff]" : "text-gray-700"}`
+                            }
+                        >
+                            {responseValue.policyName}
+                        </NavLink>
+                    </div>
+                ))}
+
+
+
+            </div>
         </div>
-      ))}
-      {selectedOptions.length === 2 && (
-        <div className="selectedOptions">
-          <p className="selectResult">Selected Options:</p>
-          <ul>
-            {selectedOptions.map((option, index) => (
-              <li key={index}>{option}</li>
-            ))}
-          </ul>
-          <button onClick={handleSubmit}>Compare</button>
-        </div>
-      )}
-    
+        {selectedPolicyDetails && (
+            <div className="selectedPolicyDetails ml-72 mt-[15rem] ">
+                <div className="viewDetailsComponents"><div className="viewDetailsHeader">
+                    <div className="cancelButtons">
+                        <div className="linees"></div>
+                        <div className="cancel" >
+                            <img src={cancel} alt="" height='25px' id='checked' /> </div>
+                    </div>
 
-{/* <Select
-        isMulti
-        options={responseData.map((responseValue) => ({
-          value: responseValue.policyName,
-          label: responseValue.policyName,
-        }))}
-        value={selectedPolicy}
-        onChange={handleSelectChange}
-      />
-      <div className="policyCompareName">
-        {selectedPolicy.map((option, index) => (
-          <div className="minCover" key={index}>
-            {option.label}
-          </div>
-        ))}
-      </div> */}
+                    <div className="policyNameDiv">
+                        <div><img className="insuranceIcon" src={insuranceIcon} alt="" height='45px' id='checked' /></div>
+                        <div className="policyNames">{selectedPolicyDetails.policyName}</div>
+                    </div>
+                    <div className="lines"></div>
+
+                </div>
+                    <div className='otherDetails'>
+                        <div className='policyDescription'>
+                            {selectedPolicyDetails.policyDescription}
+                        </div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Features</th>
+                                    <th>Details</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Policy Type</td>
+                                    <td>{selectedPolicyDetails.policyType}</td>
+
+                                </tr>
+                                {selectedPolicyDetails.premiumRate ?
+                                    <tr>
+
+                                        <td>Premium Rate</td>
+                                        <td>{selectedPolicyDetails.premiumRate / selectedPolicyDetails.policyTerm}</td>
+
+                                    </tr>
+
+                                    : null}
+                                <tr>
+                                    <td>Min Entry Age</td>
+                                    <td>{selectedPolicyDetails.minEntryAge}</td>
+
+                                </tr>
+                                <tr>
+                                    <td>Max Entry Age</td>
+                                    <td>{selectedPolicyDetails.maxEntryAge}</td>
+
+                                </tr>
+                                <tr>
+                                    <td>Expiry Age</td>
+                                    <td>{selectedPolicyDetails.expiryAge}</td>
+
+                                </tr>
+                                <tr>
+                                    <td>Min Cover</td>
+                                    <td>{selectedPolicyDetails.minCover}</td>
+
+                                </tr>
+                                {selectedPolicyDetails.maxCover ? <tr>
+                                    {/* {policyData.policyName ? <div className="">{policyData.policyName}</div>:null} */}
+                                    <td>Max Cover</td>
+                                    <td>{selectedPolicyDetails.maxCover}</td>
+
+                                </tr> : null}
+                                <tr>
+                                    <td>Payment Mode</td>
+                                    <td>{selectedPolicyDetails.paymentMode}</td>
+
+                                </tr>
+                                <tr>
+                                    <td>Policy Term</td>
+                                    <td>{selectedPolicyDetails.policyTerm}</td>
+
+                                </tr>
+                            </tbody>
+                        </table>
 
 
-    </div>
-    </div>
+                        {selectedPolicyDetails.surrenderPolicy ? <div className="surrenderLabel">
+                            <div className="descriptionLable">Surrender Policy:</div>
+                            <div>{selectedPolicyDetails.surrenderPolicy}</div>
+                        </div> : null}
 
-    
-           
-           
-            </section>
+                        {selectedPolicyDetails.riskCommencementPeriod ? <div className="risk">
+                            <div className="descriptionLable">Risk Commencement Period:</div>
+                            <div>{selectedPolicyDetails.riskCommencementPeriod}</div>
+                        </div> : null}
+
+                        {selectedPolicyDetails.riskCommencementDetails ? <div className="riskDetail">
+                            <div className="descriptionLable">Risk Commencement Details:</div>
+                            <div>{selectedPolicyDetails.riskCommencementDetails}</div>
+                        </div> : null}
+
+                        {selectedPolicyDetails.maturityBenefits ? <div className="policyType">
+                            <div className="descriptionLable">Maturity Benefits:</div>
+                            <div>{selectedPolicyDetails.maturityBenefits}</div>
+                        </div> : null}
+
+                        <div className="viewDetailsFooter">
+                            <div className="companyName">{selectedPolicyDetails.companyName}
+                                {selectedPolicyDetails.policyLaunchDate ? <div className="policyLaunchDate">
+                                    <div>Policy Launch Date : </div> {selectedPolicyDetails.policyLaunchDate.split("T")[0]}</div> : null}
+
+                            </div>
+                            <div className="contactCompany">
+                                <p className="needHelp"> Need Help?</p>
+                                <div className="contactNumber">
+                                    <div className="callImage"><img src={phoneCall} alt="" height='25px' id='checked' /></div>
+                                    <div>{selectedPolicyDetails.phoneNumber}</div>
+                                </div>
+
+                                <div className="contactNumber">
+                                    <div className="callImage"><img src={internet} alt="" height='25px' id='checked' /></div>
+                                    <div>  <a href={selectedPolicyDetails.website} target="_blank" rel="noopener noreferrer" >{selectedPolicyDetails.website}</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )}
+
+
+
+    </section>
 );
 
 }
--- einsurance.client/src/components/FilterPolicy/ViewDetail.jsx
+++ einsurance.client/src/components/FilterPolicy/ViewDetail.jsx
@@ -3,7 +3,7 @@ import Compare from './Compare';
 import { useState, useEffect } from 'react';
 import insuranceIcon from '../Icons/insuranceIcon.png';
 import './ViewDetail.css';
-import { Link,useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import cancel from '../Icons/CancelButton.png';
 import phoneCall from '../Icons/phoneCall.png';
@@ -11,10 +11,9 @@ import internet from '../Icons/internet.png';
 import { useResponse } from '../../context/ResponseContext';
 
 
-const ViewDetail = () =>
-{
+const ViewDetail = () => {
     const { policyName } = useParams();
-   /* const decodedPolicyName = decodeURIComponent(policyName);*/
+    /* const decodedPolicyName = decodeURIComponent(policyName);*/
     const { responseData } = useResponse();
     const [policyData, setPolicyData] = useState(null);
 
@@ -34,7 +33,7 @@ const ViewDetail = () =>
     }, [policyName, responseData, navigate]);
 
     if (!responseData) {
-        
+
         return <div>Loading...</div>;
     }
 
@@ -52,136 +51,140 @@ const ViewDetail = () =>
 
 
 
-      return (
+    return (
         <section className="viewDetailsMain">
-            {/* <div className="comparePolicies">
-            <Compare/>
-            </div> */}
-            <div className="viewDetailsComponents"><div className="viewDetailsHeader">
-            <div className="cancelButtons">
-                <div className="linees"></div>
-                    <div className="cancel" onClick={handleCancelButton}><img src={cancel}  alt=""       height='25px' id='checked'/> </div>
-                    </div>
-               
-        <div className="policyNameDiv">
-            <div><img className="insuranceIcon"src={insuranceIcon}  alt=""height='45px' id='checked'/></div>
-                      <div className="policyNames">{policyData.policyName}</div>
-               </div>
-               <div className="lines"></div>
-               
+            <div className="compareAndViewDetailsContainer">
+                <div className=" selectComponentDetails">
+                    <Compare />
+                </div>
+
+                <div className="viewDetailsComponents"><div className="viewDetailsHeader">
+                <div className="cancelButtons">
+                    <div className="linees"></div>
+                    <div className="cancel" onClick={handleCancelButton}><img src={cancel} alt="" height='25px' id='checked' /> </div>
+                </div>
+
+                <div className="policyNameDiv">
+                    <div><img className="insuranceIcon" src={insuranceIcon} alt="" height='45px' id='checked' /></div>
+                    <div className="policyNames">{policyData.policyName}</div>
+                </div>
+                <div className="lines"></div>
+
             </div>
-            <div className='otherDetails'>
-            <div className='policyDescription'>
-                          {policyData.policyDescription}
-               </div>
-                <table>
-                <thead>
-                <tr>
-                    <th>Features</th>
-                    <th>Details</th>
-                
-                </tr>
-                </thead>
-                    <tbody>
-                    <tr>
-                        <td>Policy Type</td>
-                                  <td>{policyData.policyType}</td>
-                    
-                    </tr>
-                    {policyData.premiumRate ?
-                    <tr>
-                     
-                       <td>Premium Rate</td>
-                                  <td>{policyData.premiumRate / policyData.policyTerm}</td> 
-                        
-                    </tr>
+                <div className='otherDetails'>
+                    <div className='policyDescription'>
+                        {policyData.policyDescription}
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Features</th>
+                                <th>Details</th>
 
-                    : null} 
-                    <tr>
-                        <td>Min Entry Age</td>
-                                  <td>{policyData.minEntryAge}</td>
-                        
-                    </tr>
-                    <tr>
-                        <td>Max Entry Age</td>
-                                  <td>{policyData.maxEntryAge}</td>
-                        
-                    </tr>
-                    <tr>
-                        <td>Expiry Age</td>
-                                  <td>{policyData.expiryAge}</td>
-                        
-                    </tr>
-                    <tr>
-                        <td>Min Cover</td>
-                                  <td>{policyData.minCover}</td>
-                        
-                    </tr>
-                              {policyData.maxCover ? <tr>  
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Policy Type</td>
+                                <td>{policyData.policyType}</td>
+
+                            </tr>
+                            {policyData.premiumRate ?
+                                <tr>
+
+                                    <td>Premium Rate</td>
+                                    <td>{policyData.premiumRate / policyData.policyTerm}</td>
+
+                                </tr>
+
+                                : null}
+                            <tr>
+                                <td>Min Entry Age</td>
+                                <td>{policyData.minEntryAge}</td>
+
+                            </tr>
+                            <tr>
+                                <td>Max Entry Age</td>
+                                <td>{policyData.maxEntryAge}</td>
+
+                            </tr>
+                            <tr>
+                                <td>Expiry Age</td>
+                                <td>{policyData.expiryAge}</td>
+
+                            </tr>
+                            <tr>
+                                <td>Min Cover</td>
+                                <td>{policyData.minCover}</td>
+
+                            </tr>
+                            {policyData.maxCover ? <tr>
                                 {/* {policyData.policyName ? <div className="">{policyData.policyName}</div>:null} */}
-                        <td>Max Cover</td>
-                                  <td>{policyData.maxCover}</td>
-                        
-                    </tr> : null}
-                    <tr>
-                        <td>Payment Mode</td>
-                                  <td>{policyData.paymentMode}</td>
-                        
-                    </tr>
-                    <tr>
-                        <td>Policy Term</td>
-                                  <td>{policyData.policyTerm}</td>
-                        
-                    </tr>
-                    </tbody>
-      </table>
-          
+                                <td>Max Cover</td>
+                                <td>{policyData.maxCover}</td>
 
-                      {policyData.surrenderPolicy? <div className="surrenderLabel">
-            <div className="descriptionLable">Surrender Policy:</div>
-                          <div>{policyData.surrenderPolicy}</div>
-        </div>:null}
-        
-                      {policyData.riskCommencementPeriod?<div className="risk">
-            <div className="descriptionLable">Risk Commencement Period:</div>
-                          <div>{policyData.riskCommencementPeriod}</div>
-        </div>: null }
+                            </tr> : null}
+                            <tr>
+                                <td>Payment Mode</td>
+                                <td>{policyData.paymentMode}</td>
 
-                      {policyData.riskCommencementDetails?  <div className="riskDetail">
-            <div className="descriptionLable">Risk Commencement Details:</div>
-                          <div>{policyData.riskCommencementDetails}</div>
-        </div> : null}
+                            </tr>
+                            <tr>
+                                <td>Policy Term</td>
+                                <td>{policyData.policyTerm}</td>
 
-        {policyData.maturityBenefits ? <div className="policyType">
-            <div className="descriptionLable">Maturity Benefits:</div>
-                          <div>{policyData.maturityBenefits}</div>
-        </div>:null}
+                            </tr>
+                        </tbody>
+                    </table>
 
-            <div className="viewDetailsFooter">
-                          <div className="companyName">{policyData.companyName}
-                              {policyData.policyLaunchDate ? <div className="policyLaunchDate">
-                                  <div>Policy Launch Date : </div> {policyData.policyLaunchDate.split("T")[0]}</div> : null}
-                          
-                </div>
-                <div className="contactCompany">
-                    <p className="needHelp"> Need Help?</p>
-                    <div className="contactNumber">
-                        <div className="callImage"><img src={phoneCall}  alt=""height='25px' id='checked'/></div>
-                                  <div>{policyData.phoneNumber}</div>
+
+                    {policyData.surrenderPolicy ? <div className="surrenderLabel">
+                        <div className="descriptionLable">Surrender Policy:</div>
+                        <div>{policyData.surrenderPolicy}</div>
+                    </div> : null}
+
+                    {policyData.riskCommencementPeriod ? <div className="risk">
+                        <div className="descriptionLable">Risk Commencement Period:</div>
+                        <div>{policyData.riskCommencementPeriod}</div>
+                    </div> : null}
+
+                    {policyData.riskCommencementDetails ? <div className="riskDetail">
+                        <div className="descriptionLable">Risk Commencement Details:</div>
+                        <div>{policyData.riskCommencementDetails}</div>
+                    </div> : null}
+
+                    {policyData.maturityBenefits ? <div className="policyType">
+                        <div className="descriptionLable">Maturity Benefits:</div>
+                        <div>{policyData.maturityBenefits}</div>
+                    </div> : null}
+
+                    <div className="viewDetailsFooter">
+                        <div className="companyName">{policyData.companyName}
+                            {policyData.policyLaunchDate ? <div className="policyLaunchDate">
+                                <div>Policy Launch Date : </div> {policyData.policyLaunchDate.split("T")[0]}</div> : null}
+
+                        </div>
+                        <div className="contactCompany">
+                            <p className="needHelp"> Need Help?</p>
+                            <div className="contactNumber">
+                                <div className="callImage"><img src={phoneCall} alt="" height='25px' id='checked' /></div>
+                                <div>{policyData.phoneNumber}</div>
+                            </div>
+
+                            <div className="contactNumber">
+                                <div className="callImage"><img src={internet} alt="" height='25px' id='checked' /></div>
+                                <div>  <a href={policyData.website} target="_blank" rel="noopener noreferrer" >{policyData.website}</a></div>
+                            </div>
+                        </div>
                     </div>
-
-                    <div className="contactNumber">
-                        <div className="callImage"><img src={internet}  alt=""height='25px' id='checked'/></div>
-                                  <div>  <a href={policyData.website} target="_blank" rel="noopener noreferrer" >{policyData.website}</a></div>
-                    </div>
                 </div>
-              </div>
-              </div>
-              </div>
+            </div>
+                
+           
+            </div>
 
-             
         </section>
-      );
+    );
 
 
 }
